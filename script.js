5e2cf0b7dbc71cb27299a20862c808bc7de2a063
Sahil Behera <sahilbehera1012007@gmail.com>
2026-06-28 13:58:38 +0530

    Implement form submission handling for quiz start
    Added event listener for form submission to handle user name input and transition screens.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+// part 3
+// pushed by: Sahil
+// Description: System execution starters and authentication form intercept handlers to transition into active views
+showBestScore();
+loadTheme();
+
+startForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    userName = username.value.trim();
+
+    if (userName === "") {
+        alert("Please enter your name");
+        return;
+    }
+
+    startScreen.classList.add("hidden");
+    quizScreen.classList.remove("hidden");
+
+    showQuestion();
+});
